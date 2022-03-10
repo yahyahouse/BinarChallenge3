00c7e3fa3561599f2addf55da31e5d3d1d2f749b
Yahya Wijaya <yahyawijaya082@gmail.com>
2022-03-10 18:55:13 +0700

    pertama

---
+++ src/main/java/Kelas.java
@@ -0,0 +1,21 @@
+
+public class Kelas {
+    private String Kelas;
+    private String Nilai;
+
+    public String getKelas() {
+        return Kelas;
+    }
+
+    public void setKelas(String kelas) {
+        Kelas = kelas;
+    }
+
+    public String getNilai() {
+        return Nilai;
+    }
+
+    public void setNilai(String nilai) {
+        Nilai = nilai;
+    }
+}
