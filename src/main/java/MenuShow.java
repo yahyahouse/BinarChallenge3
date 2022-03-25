b2f2a70cdc0f0c808143432cb2e394b580273c33
Yahya Wijaya <yahyawijaya082@gmail.com>
2022-03-25 23:23:59 +0700

    keempat tambah unit test belum selesai

--- src/main/java/MenuShow.java
+++ src/main/java/MenuShow.java
@@ -1,12 +1,14 @@
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MenuShow {
     Scanner scan = new Scanner(System.in);
-    public String file = "src/main/resources/data_sekolah.csv";
-    public String MeanMedianModus = "src/main/resources/MeanMedianModus.txt";
-    public String Modus = "src/main/resources/modus.txt";
-    public short Menu() throws IOException {
+    public static String file = "src/main/resources/data_sekolah.csv";
+    public static String MeanMedianModus = "src/main/resources/MeanMedianModus.txt";
+    public static String Modus = "src/main/resources/modus.txt";
+    public static void Menu() {
+        try {
         Scanner scan = new Scanner(System.in);
         ReadWrite readWrite = new ReadWrite();
         Menu menu = () -> {
@@ -18,20 +20,27 @@ public class MenuShow {
             System.out.println("2. Menghitung Modus Sekolah");
             System.out.println("0. Exit");
         };
-        menu.ShowMenu();
-        int  pilih = scan.nextInt();
-        switch (pilih){
-            case 1:
-                readWrite.write(MeanMedianModus);
-                break;
-            case 2:
-                readWrite.writeMod(Modus);
-                break;
-            case 0:
-                System.exit(0);
-                break;
+            menu.ShowMenu();
+            int pilih = scan.nextInt();
+            switch (pilih) {
+                case 1:
+                    readWrite.write(MeanMedianModus);
+                    break;
+                case 2:
+                    readWrite.writeMod(Modus);
+                    break;
+                case 0:
+                    System.exit(0);
+                    break;
+                default:
+                    System.err.println("Masukan pilihan yang benar!");
+                    Menu();
+            }
+        }catch (InputMismatchException | IOException e){
+            System.err.println(" Masukan pilihan dengan input angka "+ e.getMessage());
+            Menu();
         }
-        return 0;
+
     }
 }
 
--- src/test/java/menuTest.java
+++ src/test/java/menuTest.java
@@ -1,22 +1,41 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class menuTest {
-    MenuShow menuShow;
-    ReadWrite readWrite;
-
-    @BeforeEach
-    void pertama() {
-        new MenuShow();
-        new ReadWrite();
+    @Test
+    @DisplayName("Test untuk Menu Utama")
+    void MenuTest() throws IOException {
+            Menu menu = () -> {
+                System.out.println("================================");
+                System.out.println("Aplikasi Pengolah Nilai Siswa ");
+                System.out.println("================================");
+                System.out.println("File akan dibaca di <src/main/resources/data_sekolah.csv>");
+                System.out.println("1. Menghitung Mean-Median-Modus");
+                System.out.println("2. Menghitung Modus Sekolah");
+                System.out.println("0. Exit");
+            };
+            menu.ShowMenu();
+            int pilih = 1;
+            switch (pilih) {
+                case 1:
+//                    readWrite.write(MeanMedianModus);
+                    break;
+                case 2:
+//                    readWrite.writeMod(Modus);
+                    break;
+                case 0:
+//                    System.exit(0);
+                    break;
+                default:
+                    System.err.println("Masukan pilihan yang benar!");
+//                    Menu();
+            }
     }
-//    @Test
-//    void write() throws IOException {
-//        Assertions.assertDoesNotThrow(() -> menuShow.Menu(1));
-//    }
 //
 //    @Test
 //    void writeMod() throws IOException {
