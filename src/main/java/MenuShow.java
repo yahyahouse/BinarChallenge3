662dd15efea1e59215d864fff1750b9791825ab1
Yahya Wijaya <yahyawijaya082@gmail.com>
2022-03-30 17:13:15 +0700

    ketujuh tambahan try catch

--- src/main/java/MenuShow.java
+++ src/main/java/MenuShow.java
@@ -2,23 +2,26 @@ import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import static com.sun.corba.se.impl.util.Utility.printStackTrace;
+
 public class MenuShow {
     Scanner scan = new Scanner(System.in);
     public static String file = "src/main/resources/data_sekolah.csv";
     public static String MeanMedianModus = "src/main/resources/MeanMedianModus.txt";
     public static String Modus = "src/main/resources/modus.txt";
     public static void Menu() {
-        Scanner scan = new Scanner(System.in);
-        ReadWrite readWrite = new ReadWrite();
-        Menu menu = () -> {
-            System.out.println("================================");
-            System.out.println("Aplikasi Pengolah Nilai Siswa ");
-            System.out.println("================================");
-            System.out.println("File akan dibaca di <src/main/resources/data_sekolah.csv>");
-            System.out.println("1. Menghitung Mean-Median-Modus");
-            System.out.println("2. Menghitung Modus Sekolah");
-            System.out.println("0. Exit");
-        };
+        try {
+            Scanner scan = new Scanner(System.in);
+            ReadWrite readWrite = new ReadWrite();
+            Menu menu = () -> {
+                System.out.println("================================");
+                System.out.println("Aplikasi Pengolah Nilai Siswa ");
+                System.out.println("================================");
+                System.out.println("File akan dibaca di <src/main/resources/data_sekolah.csv>");
+                System.out.println("1. Menghitung Mean-Median-Modus");
+                System.out.println("2. Menghitung Modus Sekolah");
+                System.out.println("0. Exit");
+            };
             menu.ShowMenu();
             int pilih = scan.nextInt();
             switch (pilih) {
@@ -35,6 +38,10 @@ public class MenuShow {
                     System.err.println("Masukan pilihan yang benar!");
                     Menu();
             }
+        }catch (Exception e){
+            printStackTrace();
+
+        }
 
     }
 }
